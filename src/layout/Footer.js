726d6611bded69726085a7d5cb5b7046324199cb
omkarrojekar <rrcg.omkar@gmail.com>
2019-09-18 11:30:30 +0530

    footer change

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <React.Fragment>
-                    <footer className="footer text-right">
+                    <footer className="footer text-center">
                     &copy; 2018 - 2019. All rights reserved.<a href="http://riverroute.in/"> Riverroute Creative Group</a>
                     </footer>
             </React.Fragment>
